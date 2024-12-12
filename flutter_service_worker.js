da79106840289056f00b9da7f8bbcd205f810d05
BolisettySujith <bsujith116@gmail.com>
2024-12-12 12:24:23 +0530

    update for ios svg images

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -6,7 +6,7 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {"version.json": "49bdc8ee70c25a44a242081a7fa9d3e5",
 "index.html": "f53c33fc02bc4ff6aa238acc73a4d2cd",
 "/": "f53c33fc02bc4ff6aa238acc73a4d2cd",
-"main.dart.js": "3115d047cb5b3be8ca760eaab01e2020",
+"main.dart.js": "dddcb994221ca05ed9ca0cf9e6c0ead7",
 "404.html": "cc655ab8535ad1a1a6de090256172099",
 "flutter.js": "c71a09214cb6f5f8996a531350400a9a",
 "favicon.png": "965df86339ec4c6aca6d16774e9b0028",
